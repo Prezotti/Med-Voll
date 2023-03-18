44f2b0bbfdc23e8ceffbb7d644de70634fde9b07
HENRIQUE G PREZOTTI <henriqueprezotti@gmail.com>
2023-03-18 12:42:16 -0300

    Added findAll() method in PacientesController

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,14 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -23,4 +23,11 @@ public class PacienteController {
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
     }
+
+    @GetMapping
+    @Transactional
+    public Page<DadosListagemPaciente> listar(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
+
 }

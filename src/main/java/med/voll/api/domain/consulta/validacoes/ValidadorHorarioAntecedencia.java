ef5115b0fddcb3196b8a3d0129e55c44e5d952f5
HENRIQUE G PREZOTTI <henriqueprezotti@gmail.com>
2023-03-29 20:43:36 -0300

    Added scheadule validations classes

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,18 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioAntecedencia {
+    public void validar(DadosAgendamentoConsulta dados){
+        var dataConsulta = dados.data();
+        var agora = LocalDateTime.now();
+        var antecedencia = Duration.between(agora, dataConsulta).toMinutes();
+        if (antecedencia < 30){
+            throw new ValidacaoException("Consulta deve ser agendada com antecedência mínima de 30 minutos!");
+        }
+    }
+}
--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -23,4 +23,12 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             limit 1
             """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
+
+    @Query("""
+            select m.ativo
+            from Medico m
+            where
+            m.id = :id        
+            """)
+    Boolean findAtivoById(Long idMedico);
 }

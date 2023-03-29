7d7ebfee49ff4e635a51a5afc6f3968344a1bc2f
HENRIQUE G PREZOTTI <henriqueprezotti@gmail.com>
2023-03-29 20:57:29 -0300

    Implemented intarface ValidadorAgendamentoDeConsuta

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
@@ -3,9 +3,12 @@ package med.voll.api.domain.consulta.validacoes;
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public class ValidadorMedicoAtivo {
-
+@Component
+public class ValidadorMedicoAtivo implements ValidadorAgendamentoDeConsulta{
+    @Autowired
     private MedicoRepository repository;
 
     public void validar(DadosAgendamentoConsulta dados){
--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComOutraConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComOutraConsulta.java
@@ -3,9 +3,12 @@ package med.voll.api.domain.consulta.validacoes;
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public class ValidadorMedicoComOutraConsulta {
-
+@Component
+public class ValidadorMedicoComOutraConsulta implements ValidadorAgendamentoDeConsulta{
+    @Autowired
     private ConsultaRepository repository;
 
     public void validar(DadosAgendamentoConsulta dados){
